d0df042040b64c9abd1f3aae00ab55d6abbab75b
Milan <milanvpage@gmail.com>
2021-03-06 10:15:25 -0800

    commit tree 9106e7c0b124ed77faf5188f26b7cc25c886e768

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(names)# code an argument here
+  names.each_with_index do |name,index|
+    puts "#{index + 1}. #{name}"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
+  calls.map do |call|
+   "#{call.capitalize}!"
+end
+end
+
+def long_planeteer_calls(calls)# code an argument here
+  #binding.pry
+   calls.any? { |call| call.length > 4 }
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_types)# code an argument here
+  #binding.pry
+  cheeses = ["cheddar", "gouda", "camembert"]
+  cheese_types.find do |cheese|
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheeses.include?(cheese)
+end
 end
